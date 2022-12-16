863575bd7658d7dbf6e2bc8f877e821094728b85
PierreGoupil13 <p.goupil356@gmail.com>
2022-12-16 11:14:54 +0100

    Correction du bug du numéro de la caisse. Création de la page html de single view d'une bouteille. Création de la page d'acceuil avec la liste des vins présent dans la cave. Finalisation de la requete de stockage de toutes les informations d'une bouteille. Création du routage pour passer de l'acceuil à la page de single view, passage d'info par l'url pour faire cela.
    Fin de la version 1.0, prêt pour la mise en production.

--- CaveAVins/website/views.py
+++ CaveAVins/website/views.py
@@ -7,8 +7,8 @@ views = Blueprint('views', __name__)
 
 @views.route('/', methods=['POST', 'GET'])
 def home():
-    vin = Bouteilles.query.filter_by(id = 1).first()
-    return render_template("home.html", vin=vin)
+    cave = db.session.query(Bouteilles).all()
+    return render_template("home.html", cave=cave)
 
 @views.route('/ajouter', methods=['POST', 'GET'])
 def ajouter():
@@ -16,11 +16,24 @@ def ajouter():
         # Get data from form
         nom_vin = request.form.get('nameBout')
         annee_prod = request.form.get('annee')
-        num_caisse = request.form.get('numCais')
+        num_caisse = request.form.get('numCaisse')
+        type_vin = request.form.get('type')
+        producteur = request.form.get('producteur')
+        num_bouteille = request.form.get('nombTeil')
+        note_vin = request.form.get('note')
+        comment = request.form.get('comment')
 
-        new_bouteille = Bouteilles(nom_vin = nom_vin, annee=annee_prod, num_caisse=num_caisse)
+        # Instaurer du controle sur les infos maintenant
+
+
+        new_bouteille = Bouteilles(
+            num_caisse=num_caisse, nombre=num_bouteille, nom_vin=nom_vin, annee=annee_prod, commentaire_vin=comment, note_vin=note_vin,type_vin=type_vin,producteur_vin=producteur
+            )
         db.session.add(new_bouteille)
         db.session.commit()
-
-        print(annee_prod,num_caisse)
     return render_template("ajouter.html")
+
+@views.route('/single/<int:id>', methods=['POST','GET'])
+def single(id):
+    vin = db.session.query(Bouteilles).filter_by(id=id).first()
+    return render_template("single.html", vin=vin)
